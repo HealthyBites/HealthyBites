9d5fc53652ed7da5064170e1bff7d237933af763
Osmany Pujol <Osmany.Pujolcs@gmail.com>
2021-11-07 17:14:39 -0500

    added the dependency for the navagation bar

--- app/src/main/java/com/osmany/healthybites/MainActivity.java
+++ app/src/main/java/com/osmany/healthybites/MainActivity.java
@@ -13,8 +13,6 @@ import com.osmany.healthybites.data.models.RecipieList;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 
 public class MainActivity extends AppCompatActivity {
