d9b55a15dfc0d998e1f9fe4dea38d53199acd82d
Osmany Pujol <Osmany.Pujolcs@gmail.com>
2021-11-07 17:34:31 -0500

    organized code

--- app/src/main/java/com/osmany/healthybites/MainActivity.java
+++ app/src/main/java/com/osmany/healthybites/MainActivity.java
@@ -16,33 +16,13 @@ import retrofit2.Response;
 
 
 public class MainActivity extends AppCompatActivity {
-    TextView tvTitle;
     String baseUrl = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tvTitle = findViewById(R.id.tvHello);
 
-        JsonPlaceHolderApi jsonPlaceHolderApi = RetrofitClient.getClient(baseUrl).create(JsonPlaceHolderApi.class);
-
-        Call<RecipieList> call = jsonPlaceHolderApi.getRecipies();
-        call.enqueue(new Callback<RecipieList>() {
-            @Override
-            public void onResponse(Call<RecipieList> call, Response<RecipieList> response) {
-                if(!response.isSuccessful()){
-                    tvTitle.setText("Code: " + response.code());
-                    return;
-                }
-                tvTitle.setText(response.body().getRecipe().get(0).getTitle());
-            }
-
-            @Override
-            public void onFailure(Call<RecipieList> call, Throwable t) {
-                tvTitle.setText(t.getMessage());
-            }
-        });
     }
 
 }
