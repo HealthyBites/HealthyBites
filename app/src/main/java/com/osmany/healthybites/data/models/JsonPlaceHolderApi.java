f3d54a0752a3a759178b10d856fced42bd9302d5
Osmany Pujol <Osmany.Pujolcs@gmail.com>
2021-11-08 00:20:43 -0500

    Finished the MainActivity stream

--- app/src/main/java/com/osmany/healthybites/data/models/JsonPlaceHolderApi.java
+++ app/src/main/java/com/osmany/healthybites/data/models/JsonPlaceHolderApi.java
@@ -1,7 +1,6 @@
 package com.osmany.healthybites.data.models;
 
 import com.osmany.healthybites.BuildConfig;
-import com.osmany.healthybites.data.models.RecipieList;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
@@ -11,5 +10,5 @@ public interface JsonPlaceHolderApi {
     @Headers({"x-rapidapi-host:" + BuildConfig.HOST,
             "x-rapidapi-key:" + BuildConfig.KEY})
     @GET("recipes/random?number=10")
-    Call<RecipieList> getRecipies();
+    Call<RecipeList> getRecipies();
 }
