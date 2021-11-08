5645ddd52c03646725201f16b9b9f19dd89e4cc6
JF5595 <51301956+JF5595@users.noreply.github.com>
2021-11-08 00:41:39 -0500

    Added placeholder activity navigation between the login and main activities

--- app/src/main/java/com/osmany/healthybites/LoginActivity.java
+++ app/src/main/java/com/osmany/healthybites/LoginActivity.java
@@ -1,13 +1,37 @@
 package com.osmany.healthybites;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button btLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btLogin = findViewById(R.id.btLogin);
+
+
+      btLogin.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+              openMainActivity();
+          }
+      });
+
+
+    }
+
+
+    private void openMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
